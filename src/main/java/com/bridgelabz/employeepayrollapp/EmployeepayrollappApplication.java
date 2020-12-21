b195562e15f5323bd1228747ccd1052ccc6eb991
dvranga <dvranga@users.noreply.github.com>
2020-12-22 00:10:54 +0530

    [ranganath]Added:Lombok library for logging where logging levels, files and patterns are handled in application.properties

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeepayrollappApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeepayrollappApplication.java
@@ -2,12 +2,18 @@ package com.bridgelabz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeepayrollappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeepayrollappApplication.class, args);
+		ApplicationContext context= SpringApplication.run(EmployeepayrollappApplication.class, args);
+		log.info(context.getEnvironment().getProperty("environment"));
+		log.info(context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
