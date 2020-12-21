683a5427708e65744de5f3c5e0a5c532a8b147d6
dvranga <dvranga@users.noreply.github.com>
2020-12-22 04:16:49 +0530

    [ranganath]Added:Added validation to name field so the rest call can be validated

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,21 +1,21 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+	
+	@Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$", message="Employee name Invalid")
+	@NotEmpty(message="Employee name cannot be null ")
 	public String name;
+	
+	@Min(value = 500, message="Min wage should be more than 500 ")
 	public long salary;
+	
 	public EmployeePayrollDTO(String name, long salary) {
 		super();
 		this.name = name;
 		this.salary = salary;
-	}
-	
-	
-	@Override
-	public String toString() {
-		return "EmployeePayrollDTO [name=" + name + ", salary=" + salary + "]";
-	}
-	
-	
-	
-	
+	}	
 }
